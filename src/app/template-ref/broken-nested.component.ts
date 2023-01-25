a678840c7765d7ddf1faf358d14de4bdd391b2ae
rsescu <r.sescu@gmail.com>
2023-01-25 14:44:58 +0200

    Fix injectino for broken-nested component

--- src/app/template-ref/broken-nested.component.ts
+++ src/app/template-ref/broken-nested.component.ts
@@ -1,14 +1,14 @@
 import { Component } from '@angular/core';
+import { FarmComponent } from './farm';
 
 @Component({
-    selector: 'my-broken-nested',
-    template: `
+  selector: 'my-broken-nested',
+  template: `
 
         <div>
             <ng-content></ng-content>
         </div>
     `,
+  providers: [{ provide: FarmComponent, useExisting: BrokenNestedComponent }],
 })
-export class BrokenNestedComponent {
-
-}
+export class BrokenNestedComponent extends FarmComponent {}
--- src/app/template-ref/template-ref-broken.component.ts
+++ src/app/template-ref/template-ref-broken.component.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
 
 @Component({
-    selector: 'my-template-ref-broken',
-    template: `
+  selector: 'my-template-ref-broken',
+  template: `
         <my-broken-nested>
-            <button *mySheepBlueprint></button>
+            <button *mySheepBlueprint>Some other Button</button>
         </my-broken-nested>
-    `
+    `,
 })
-export class TemplateRefBrokenComponent {
-}
+export class TemplateRefBrokenComponent {}
